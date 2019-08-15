e92793a5070a6eafbd18f1ec4cc0abaaf2d55a00
Tom Slade <51721274+tomslade1@users.noreply.github.com>
2019-08-15 17:34:52 +0100

    Split into Stream & Rest files

--- dc_stream_class_creation.py
+++ dc_stream_class_creation.py
@@ -10,10 +10,9 @@ C:\\Users\\TSL03\\OneDrive - Sky\\Documents\\Python Working Area\\\
 Python_Code\\Project_Notebooks\\Twitter_Sentiment_Analysis\\Twitter_Keys.json"
 
 with open(json_path) as json_file:
-    json_data = json.load(json_file)
+	json_data = json.load(json_file)
 
 creds = json_data['Twitter_API_Credentials']
-
 
 access_token = creds['access_token']
 access_token_secret = creds['access_token_secret']
@@ -30,9 +29,9 @@ api = tweepy.API(auth)
 # Showing an example REST API method - home_timeline grabs top 20 most recent
 # tweets from defined account's feed
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
 
 # Example of using the REST API to access User acct info
 
@@ -45,6 +44,22 @@ for tweet in public_tweets:
 
 # # Pass all attributes into a dictionary
 # print(other_user._json)
+
+
+search_results = api.search(q="nowtv -filter:retweets AND -filter:replies",
+	tweet_mode='extended', count=10)
+
+i = 0
+
+for tweet in search_results:
+	
+	if tweet.user.followers_count > 0:
+		i += 1
+		print("Tweet Number:"+str(i), "User:"+tweet.user.screen_name, '"'+
+			tweet.full_text, '"\n')
+	else:
+		pass
+
 
 
 # class MyStreamListener(tweepy.StreamListener):
